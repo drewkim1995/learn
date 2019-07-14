85019b9d5853011307b07656c9267720a8822627
Andrew <52646640+drewkim1995@users.noreply.github.com>
2019-07-14 13:22:20 +1000

    Update 4-median-of-two-sorted-arrays.cpp
    4-median-of-two-sorted-arrays: trying new method.

--- 4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays.cpp
@@ -1,72 +1,109 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 
-class Solution {
+class Solution
+{
     public:
         double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
         {
             double ret = 0.0;
-            int combinedSize = nums1.size() + nums2.size();
+
             int start1 = 0;
             int start2 = 0;
-            int end1 = nums1.size() - 1;
-            int end2 = nums2.size() - 1;
+            int end1 = nums1.size();
+            int end2 = nums2.size();
 
-            if (combinedSize == 1)
+            int remaining = end1+end2;
+            int maxLoop = (remaining / 2) - 1;
+
+            while (remaining > 1)
             {
-                if(nums1.size() == 0)
-                    ret = nums2[0];
-                else
-                    ret = nums1[0];
-            }
-
-            while (combinedSize > 1)
-            {
-                int comp1 = 0;
-                int comp2 = 0;
-
-                // Get rid of lowest first
-                if (start1 < nums1.size())
+                // Remove lowest value
+                if (start1 < end1 && start2 < end2)
                 {
-                    comp1 = nums1[start1];
-                }
-                else
-                {
-                    comp1 = nums2[start2++];
-                }
-
-                if (start2 < nums2.size())
-                {
-                    comp2 = nums2[start2];
-
-                    if (comp1 < comp2)
+                    if (nums1[start1] < nums2[start2])
+                    {
+                        if (remaining % 2 == 0)
+                            ret = (nums1[start1] + nums2[start2]) / 2;
+                        else
+                            ret = nums2[start2];
                         start1++;
+                    }
                     else
+                    {
+                        if (remaining % 2 == 0)
+                            ret = (nums1[start1] + nums2[start2]) / 2;
+                        else
+                            ret = nums1[start1];
                         start2++;
+                    }
                 }
-                else
+                else if (start1 >= end1)
                 {
-                    comp2 = nums1[++start1];
-                }
-
-                cout << "start1 = " << start1 << " | start2 = " << start2 << endl;
-                cout << "comp1 = " << comp1 << " | comp2 = " << comp2 << endl;
-
-                if (combinedSize % 2 == 0)
-                    ret = (double) (comp1 + comp2) / 2;
-                else
-                {
-                    if (comp1 < comp2)
-                        ret = comp2;
+                    start2++;
+                    ret = nums2[start2];
+                    /*
+                    if (start2 < end2)
+                        ret = (nums2[start2-1] + nums2[start2]) / 2;
                     else
-                        ret = comp1;
+                    {
+                        ret = nums2[start2-1];
+                        break;
+                    }*/
                 }
-
-                // MAYBE FIX THIS PRETEND....
-                // Pretend as if we also got rid of highest numbers
-                combinedSize -= 2;
+                else if (start2 >= end2)
+                {
+                    start1++;
+                    ret = nums1[start1];
+                    /*
+                    if (start1 < end1)
+                        ret = (nums1[start1-1] + nums1[start1]) / 2;
+                    else
+                    {
+                        ret = nums1[start1-1];
+                        break;
+                    }*/
+                }
+                cout << remaining << " loop " << start1 << "|" << end1 << " | " << start2 << "|" << end2 << endl;
+                remaining -= 2;
             }
 
-            cout << "combined size = " << combinedSize << " | ret = " << ret << endl;
+            cout << "start1 = " << start1 << " | end1 = " << end1 << endl;
+            cout << "start2 = " << start2 << " | end2 = " << end2 << endl << endl;
+
+            if ((end1+end2) % 2 != 0)
+            {
+                // Odd
+                if (start1 >= end1)
+                    ret = nums2[start2];
+                else if (start2 >= end2)
+                    ret = nums1[start1];
+                else
+                    ret = nums1[start1] < nums2[start2] ? nums1[start1] : nums2[start2];
+            }
+            else
+            {
+                cout << "hello ";
+            cout << "start1 = " << start1 << " | end1 = " << end1 << endl;
+            cout << "start2 = " << start2 << " | end2 = " << end2 << endl << endl;
+                // Even
+                if (start1 >= end1)
+                {
+                    if (start2 == 0)
+                        ret = (double) (nums1[end1-1] + nums2[start2]) / 2;
+                    else
+                        ret = (double) (nums2[start2-1] + nums2[start2]) / 2;
+                }
+                else if (start2 >= end2)
+                {
+                    if (start1 == 0)
+                        ret = (double) (nums2[end2-1] + nums1[start1]) / 2;
+                    else
+                        ret = (double) (nums1[start1-1] + nums1[start1]) / 2;
+                }
+                else
+                    ret = (double) (nums1[start1] + nums2[start2]) / 2;
+
+            } // If I stop 1 loop earlier, just add the results of [start1] and [start2]?
 
             return ret;
         }
