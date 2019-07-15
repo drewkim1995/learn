1ce4918105545ba5521a59256bea67ad5809afa0
Andrew <52646640+drewkim1995@users.noreply.github.com>
2019-07-15 15:38:28 +1000

    Create 27-remove-element.cpp
    remove-element: Initial commit.

---
+++ 27-remove-element.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/remove-element/
+
+class Solution
+{
+    public:
+        int removeElement(vector<int>& nums, int val)
+        {
+            int index = 0;
+
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] != val)
+                {
+                    if (index != i)
+                        nums[index] = nums[i];
+                    index++;
+                }
+            }
+            return index;
+        }
+};
