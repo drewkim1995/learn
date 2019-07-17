7030e59a6ec8927e8580abfad6941bc2cc47c098
Andrew <52646640+drewkim1995@users.noreply.github.com>
2019-07-18 00:53:21 +1000

    Update 112-path-sum.cpp
    path-sum: Added as comment a cleaner solution.

--- 112-path-sum.cpp
+++ 112-path-sum.cpp
@@ -10,6 +10,25 @@
  * };
  */
 
+/**
+ * Found better solution online:
+class Solution
+{
+    public:
+        bool hasPathSum(TreeNode* root, int sum)
+        {
+            if(root == NULL){
+                return false;
+            }
+            int newsum = sum - root->val;
+            if(root->left == NULL && root->right == NULL){
+                return newsum == 0;
+            }
+            return hasPathSum(root->left, newsum) || hasPathSum(root->right, newsum);
+        }
+};
+*/
+
 class Solution
 {
     private:
