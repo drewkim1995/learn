8ad94ac2ab00c8d4512b40968797ea8ecc57ee6a
Andrew <52646640+drewkim1995@users.noreply.github.com>
2019-07-17 20:28:10 +1000

    Update 118-pascals-triangle.cpp
    pascals-triangle: Cleaner solution

--- 118-pascals-triangle.cpp
+++ 118-pascals-triangle.cpp
@@ -5,24 +5,15 @@ class Solution
     public:
         vector<vector<int>> generate(int numRows)
         {
-            vector<vector<int>> ret = {{1},{1,1}};
+            vector<vector<int>> ret;
 
-            if (numRows < 1)
-                return {};
-            else if (numRows == 1)
-                return {{1}};
-
-            for (int row = 2; row < numRows; row++)
+            for (int row = 1; row <= numRows; row++)
             {
-                vector<int> v;
+                // Fill Constructor
+                vector<int> v(row, 1);
 
-                for (int col = 0; col < row+1; col++)
-                {
-                    if (col == 0 || col == row)
-                        v.push_back(1);
-                    else
-                        v.push_back(ret[row-1][col-1] + ret[row-1][col]);
-                }
+                for (int col = 1; col < row-1; col++)
+                    v[col] = ret[row-2][col-1] + ret[row-2][col];
 
                 ret.push_back(v);
             }
