3f005eb99034e2f2c7f83c018d489e271d4bb9de
Andrew <52646640+drewkim1995@users.noreply.github.com>
2019-09-01 08:45:30 +1000

    majority-element: Much simple version

--- 169-majority-element.cpp
+++ 169-majority-element.cpp
@@ -5,28 +5,7 @@ class Solution
     public:
         int majorityElement(vector<int>& nums)
         {
-            map<int, int> elements;
-
-            for (auto i : nums)
-            {
-                if (elements.count(i) == 0)
-                    elements[i] = 1;
-                else
-                    elements[i]++;
-            }
-
-            int ret = 0;
-            int count = 0;
-
-            for (auto e : elements)
-            {
-                if (e.second > count)
-                {
-                    count = e.second;
-                    ret = e.first;
-                }
-            }
-
-            return ret;
+            sort(nums.begin(), nums.end());
+            return nums[nums.size() / 2];
         }
 };
