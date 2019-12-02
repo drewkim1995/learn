ccf3e99d4734d8e4cbae4f0efe91e0e5b5ed9091
Andrew <drewkim1995@gmail.com>
2019-12-02 14:44:29 +1100

    single-number: Initial commit

---
+++ 136-single-number.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/single-number/
+
+class Solution
+{
+    private:
+        unordered_map<int, int> check;
+    public:
+        int singleNumber(vector<int>& nums)
+        {
+            // Empty or Equal Length Strings invalid
+            if (nums.size() % 2 == 0)
+                return 0;
+
+            // Sort
+            sort(nums.begin(), nums.end());
+
+            // Check every second pair due to sorting
+            for (int iter = 1; iter < nums.size(); iter+=2)
+            {
+                int prev = iter-1;
+                if (nums[prev] != nums[iter])
+                    return nums[prev];
+            }
+
+            // Last number left is the solo
+            return nums[nums.size()-1];
+        }
+};
