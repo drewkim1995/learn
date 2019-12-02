9b3d8315a13e9ebede6443bb71d11482ab22a598
Andrew <drewkim1995@gmail.com>
2019-12-02 14:52:55 +1100

    single-number: found a much better solution

--- 136-single-number.cpp
+++ 136-single-number.cpp
@@ -5,22 +5,15 @@ class Solution
     public:
         int singleNumber(vector<int>& nums)
         {
-            // Empty or Equal Length Strings invalid
-            if (nums.size() % 2 == 0)
-                return 0;
+            int single = 0;
 
-            // Sort
-            sort(nums.begin(), nums.end());
+            /* XOR:
+                If we take XOR of zero and some bit, it will return that bit.
+                If we take XOR of two same bits, it will return 0.
+            */
+            for (auto i : nums)
+                single ^= i;
 
-            // Check every second pair due to sorting
-            for (int iter = 1; iter < nums.size(); iter+=2)
-            {
-                int prev = iter-1;
-                if (nums[prev] != nums[iter])
-                    return nums[prev];
-            }
-
-            // Last number left is the solo
-            return nums[nums.size()-1];
+            return single;
         }
 };
