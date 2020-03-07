f6ee9024325143abcc042380d9483d83e4813b72
Andrew <drewkim1995@gmail.com>
2020-03-07 17:31:24 +1100

    permutations-ii: slightly faster code
    retrieved form discussion; this solution copies the vector

--- 47-permutations-ii.cpp
+++ 47-permutations-ii.cpp
@@ -4,41 +4,28 @@ class Solution
 {
     private:
         vector<vector<int>> permutations;
-        void backtrack(vector<int>& nums, int pos)
+        void recursion(vector<int> nums, int pos)
         {
             if (pos == nums.size()-1)
             {
                 permutations.push_back(nums);
                 return;
             }
+
             for (int i = pos; i < nums.size(); i++)
             {
-                if (i < nums.size()-1 && nums[i] == nums[i+1])
+                if (pos != i && nums[pos] == nums[i])
                     continue;
-                /*
-                cout << "[";
-                for (auto c : nums)
-                    cout << c << " ";
-                cout << "], pos = " << pos << endl;*/
-                swap(nums[i], nums[pos]);
-                backtrack(nums, pos+1);
-                swap(nums[i], nums[pos]);
+                swap(nums[pos], nums[i]);
+                recursion(nums, pos+1);
             }
         }
+
     public:
-        vector<vector<int>> permuteUnique(vector<int>& nums)
+        vector<vector<int>> permuteUnique(vector<int> &nums)
         {
-            if (nums.size() < 2)
-                permutations.push_back(nums);
-            else
-            {
-                sort(nums.begin(), nums.end());
-                backtrack(nums, 0);
-
-                sort(permutations.begin(), permutations.end());
-                permutations.erase(unique(permutations.begin(), permutations.end()), permutations.end());
-            }
-
+            sort(nums.begin(), nums.end());
+            recursion(nums, 0);
             return permutations;
         }
 };
