70769f97a94e5487149ca4817684b1effe444d1d
Andrew <drewkim1995@gmail.com>
2020-03-09 22:51:14 +1100

    jump-game-ii: much faster version
    faster solution from https://leetcode.com/problems/jump-game-ii/discuss/18207/Sharing-my-straightforward-C%2B%2B-solution

--- 45-jump-game-ii.cpp
+++ 45-jump-game-ii.cpp
@@ -2,48 +2,34 @@
 
 class Solution
 {
-    private:
-        unordered_map<int, int> jumpMap;
-
-        int calcJump(vector<int>& nums, int pos, int count)
-        {
-            int size = nums.size()-1;
-            if (pos == size)
-                return count;
-            else if (pos > size)
-                return INT_MAX;
-
-            int lowestCount = INT_MAX;
-
-            //cout << "nums[" << pos << "] = " << nums[pos] << " | size-pos = " << size-pos << endl;
-            // Best case would exit here with 1 jump
-            if (nums[pos] > size-pos)
-            {
-                jumpMap[pos] = count + 1;
-                return count+1;
-            }
-
-            for (int i = min(nums[pos], size-pos); i > 0; i--)
-            //for (int i = 1; i <= min(nums[pos], size-pos); i++)
-            {
-                // Not in map yet
-                if (jumpMap[pos+i] == 0)
-                    jumpMap[pos+i] = calcJump(nums, pos + i, 1);
-
-                //cout << "jumpMap[" << pos+i << "] = " << jumpMap[pos+i] << endl;
-
-                if (jumpMap[pos+i] != INT_MAX && jumpMap[pos+i] + count < lowestCount)
-                    lowestCount = jumpMap[pos+i] + count;
-
-                // 2nd Best case is 2 jumps
-                if (lowestCount == 2)
-                    break;
-            }
-            return lowestCount;
-        }
     public:
-        int jump(vector<int>& nums)
+        int jump(vector<int> & nums)
         {
-            return calcJump(nums, 0, 0);
+            int n = nums.size();
+            if(n == 0)
+                return 0;
+
+            // maxReachPos indicates the farthest reachable position.
+            int maxReachPos = nums[0];
+
+            // curMaxReachPos indicates the current farthest reachable position.
+            int curMaxReachPos = nums[0];
+
+            int curStep = 1;
+            for(int i = 1; i <= min(n-1, maxReachPos); i++)
+            {
+                curMaxReachPos = max(curMaxReachPos, i + nums[i]);
+
+                // Returns min step
+                if(i == n - 1)
+                    return curStep;
+
+                if(i == maxReachPos)
+                {
+                    maxReachPos = curMaxReachPos;
+                    curStep++;
+                }
+            }
+            return 0;
         }
 };
