098580b7a263a82ab40f8b7a4fa273d718ae9d28
Andrew <drewkim1995@gmail.com>
2020-03-11 16:28:31 +1100

    n-queens: improved solution
    Reduced run-time from 52ms to 20ms.
    Reduced memory from 15.8MB to 9.6MB.

--- 51-n-queens.cpp
+++ 51-n-queens.cpp
@@ -3,97 +3,74 @@
 // Attempting Row by Row solution so we don't need to keep
 // track of row number/count as we'll know because of vector.push_back()
 
+// Note: Avoid using vector<bool> as it is not a STL container
+
 class Solution
 {
     private:
         vector<vector<string>> result;
-        void backtrack(int n, vector<int> & qCol, vector<vector<bool>> & board)
+        unsigned char * qCol;
+        unsigned char * temp;
+
+        void backtrack(const int n, const int qColSize)
         {
-            int size = qCol.size();
+            // Vertical Check board is valid
+            memcpy(temp, qCol, sizeof(unsigned char) * qColSize);
+            sort(temp, temp+qColSize);
+            for (int i = 1; i < qColSize; i++)
+            {
+                if (temp[i] == temp[i-1])
+                    return;
+            }
+
+            // Diagonal Check board
+            for (int row = 0; row < qColSize; row++)
+            {
+                // Top-left to bottom-right
+                int col = qCol[row] + 1;
+                for (int next = row+1; next < qColSize; next++, col++)
+                    if (col == qCol[next])
+                        return;
+
+                // Top-right to bottom-left
+                col = qCol[row] + 1;
+                for (int next = row-1; next >= 0; next--, col++)
+                    if (col == qCol[next])
+                        return;
+            }
 
             // Insert solution
-            if (size == n)
+            if (n == qColSize)
             {
-                vector<string> solution;
+                vector<string> solution (n, string(n, '.'));
 
                 // Generate string here
                 for (int i = 0; i < n; i++)
-                {
-                    string line(n, '.');
-                    line[qCol[i]] = 'Q';
-                    solution.push_back(line);
-                }
+                    solution[i][qCol[i]] = 'Q';
 
                 result.push_back(solution);
                 return;
             }
 
             // Inserting a queen
-            int row = size;
-            int start = 0;
-            for (int col = start; col < n; col++)
+            for (int col = 0; col < n; col++)
             {
-                // Verify safe location to insert
-                if (!board[row][col])
-                    continue;
-
-                // Updated board
-                vector<vector<bool>> temp = board;
-                updateBoard(temp, false, n, row, col);
-
-                // Actual Inserting queen
-                qCol.push_back(col);
-                backtrack(n, qCol, temp);
-                qCol.pop_back();
-
-                //updateBoard(board, true, n, row, col);
+                qCol[qColSize] = col;
+                backtrack(n, qColSize+1);
+                // Don't need to 'reset' since we keep track via qColSize
             }
 
             return;
         }
-        void updateBoard(vector<vector<bool>> & board, bool val, int n, int row, int col)
-        {
-            // Updated board
-            for (int i = 0; i < n; i++)
-            {
-                // Vert and Horz
-                board[row][i] = val;
-                board[i][col] = val;
-
-                // Diagonal Top-Left
-                int r = row-i;
-                int c = col-i;
-                if(r >= 0 && c >= 0)
-                    board[r][c] = val;
-
-                // Diagonal Top-Right
-                //r = row-i;
-                c = col+i;
-                if(r >= 0 && c <= n-1)
-                    board[r][c] = val;
-
-                // Diagonal Bottom-Left
-                r = row+i;
-                c = col-i;
-                if(r <= n-1 && c >= 0)
-                    board[r][c] = val;
-
-                // Diagonal Bottom-Right
-                r = row+i;
-                c = col+i;
-                if(r <= n-1 && c <= n-1)
-                    board[r][c] = val;
-            }
-        }
 
     public:
-        vector<vector<string>> solveNQueens(int n)
+        vector<vector<string>> solveNQueens(const int n)
         {
-            vector<bool> row(n, true);
-            vector<vector<bool>> board (n, row);
+            // Allocated on the stack implies that it will automatically be freed when the stack unwinds.
+            qCol = (unsigned char *) alloca(sizeof(unsigned char) * n);
+            temp = (unsigned char *) alloca(sizeof(unsigned char) * n);
 
-            vector<int> queensCol;
-            backtrack(n, queensCol, board);
+            backtrack(n, 0);
             return result;
         }
 };
