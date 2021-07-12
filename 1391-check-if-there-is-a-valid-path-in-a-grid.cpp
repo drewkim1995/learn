0e26ba8a97a2dae2edae667d98109e0ccd4904ba
Andrew <drewkim1995@gmail.com>
2021-07-13 00:46:18 +1000

    check-if-there-is-a-valid-path-in-a-grid: different way

--- 1391-check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,38 +1,19 @@
 // https://leetcode.com/problems/check-if-there-is-a-valid-path-in-a-grid/
 
+enum Direction { up, left, down, right, invalid };
+struct Data {
+    Data() : inDir(invalid) {}
+    Data(Direction in, int X, int Y) : inDir(in), x(X), y(Y) {}
+    Direction inDir;
+    int x;
+    int y;
+};
+
 class Solution {
 private:
-    vector<vector<int>> grid;
-    const enum Direction { up, left, down, right, size };
+    static const Data data[6][invalid];
 
-public:
-    bool hasValidPath(vector<vector<int>>& in) {
-        if (in.empty()) return false;
-        if (in.size() == 1 && in[0].size() == 1) return true;
-
-        grid = in;
-
-        switch (grid[0][0]) {
-            case 1:
-            case 6:
-                return validPath(left, 0, 1);
-            case 2:
-            case 3:
-                return validPath(up, 1, 0);
-            case 4:
-                // Special case: 2 potential streets
-                return validPath(up, 1, 0) || validPath(left, 0, 1);
-            case 5:
-            default:
-                // Special case: if size > 1, then it will never be true
-                return false;
-        }
-
-        return false;
-    }
-
-private:
-    bool validPath(Direction inDir, int x, int y) {
+    bool validPath(const vector<vector<int>>& grid, Direction inDir, int x, int y) {
         int xEnd = grid.size() - 1;
         int yEnd = grid[0].size() - 1;
 
@@ -44,95 +25,53 @@ private:
             if (visited[x][y] != 0) return false;
             visited[x][y] = 1;
 
-            Direction out;
             // Evaluate Direction To Go
-            switch (grid[x][y]) {
-                case 1:
-                    if (inDir == left) {
-                        if (x == xEnd && y == yEnd) return true;
-                        ++y;
-                        out = right;
-                    }
-                    else if (inDir == right) {
-                        if (x == xEnd && y == yEnd) return true;
-                        --y;
-                        out = left;
-                    }
-                    else return false;
-                    break;
-                case 2:
-                    if (inDir == up) {
-                        if (x == xEnd && y == yEnd) return true;
-                        ++x;
-                        out = down;
-                    }
-                    else if (inDir == down) {
-                        if (x == xEnd && y == yEnd) return true;
-                        --x;
-                        out = up;
-                    }
-                    else return false;
-                    break;
-                case 3:
-                    if (inDir == left) {
-                        if (x == xEnd && y == yEnd) return true;
-                        ++x;
-                        out = down;
-                    }
-                    else if (inDir == down) {
-                        if (x == xEnd && y == yEnd) return true;
-                        --y;
-                        out = left;
-                    }
-                    else return false;
-                    break;
-                case 4:
-                    if (inDir == right) {
-                        if (x == xEnd && y == yEnd) return true;
-                        ++x;
-                        out = down;
-                    }
-                    else if (inDir == down) {
-                        if (x == xEnd && y == yEnd) return true;
-                        ++y;
-                        out = right;
-                    }
-                    else return false;
-                    break;
-                case 5:
-                    if (inDir == left) {
-                        if (x == xEnd && y == yEnd) return true;
-                        --x;
-                        out = up;
-                    }
-                    else if (inDir == up) {
-                        if (x == xEnd && y == yEnd) return true;
-                        --y;
-                        out = left;
-                    }
-                    else return false;
-                    break;
-                case 6:
-                    if (inDir == right) {
-                        if (x == xEnd && y == yEnd) return true;
-                        --x;
-                        out = up;
-                    }
-                    else if (inDir == up) {
-                        if (x == xEnd && y == yEnd) return true;
-                        ++y;
-                        out = right;
-                    }
-                    else return false;
-                    break;
-                default:
-                    return false;
-            }
+            Data result = data[grid[x][y]-1][inDir];
 
-            // Flip it for next cycle
-            inDir = (Direction)((out + 2) % size);
+            // Check for valid result
+            if (result.inDir == invalid) return false;
+
+            // If currently valid and at the end
+            if (x == xEnd && y == yEnd) return true;
+
+            x += result.x;
+            y += result.y;
+            inDir = result.inDir;
         }
 
         return false;
     }
+
+public:
+    bool hasValidPath(const vector<vector<int>>& grid) {
+        if (grid.empty()) return false;
+        if (grid.size() == 1 && grid[0].size() == 1) return true;
+
+        switch (grid[0][0]) {
+            case 1:
+            case 6:
+                return validPath(grid, Direction::left, 0, 1);
+            case 2:
+            case 3:
+                return validPath(grid, up, 1, 0);
+            case 4:
+                // Special case: 2 potential streets
+                return validPath(grid, up, 1, 0) || validPath(grid, Direction::left, 0, 1);
+            case 5:
+            default:
+                // Special case: if size > 1, then it will never be true
+                return false;
+        }
+
+        return false;
+    }
+};
+
+const Data Solution::data[6][invalid] =  {
+    {Data(), Data(Direction::left, 0, 1), Data(), Data(Direction::right, 0, -1)},
+    {Data(Direction::up, 1, 0), Data(), Data(Direction::down, -1, 0), Data()},
+    {Data(), Data(Direction::up, 1, 0), Data(), Data(Direction::right, 0, -1)},
+    {Data(), Data(), Data(Direction::left, 0, 1), Data(Direction::up, 1, 0)},
+    {Data(Direction::right, 0, -1), Data(Direction::down, -1, 0), Data(), Data()},
+    {Data(Direction::left, 0, 1), Data(), Data(), Data(Direction::down, -1, 0)},
 };
