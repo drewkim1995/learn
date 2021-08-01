716a9cfb72c5585265b54ebcb182634a182b8c53
Andrew <drewkim1995@gmail.com>
2021-08-01 15:27:03 +1000

    grumpy-bookstore-owner: better + shorter solution

--- leetcode/1052-grumpy-bookstore-owner.cpp
+++ leetcode/1052-grumpy-bookstore-owner.cpp
@@ -1,4 +1,4 @@
-/*
+/* https://leetcode.com/problems/grumpy-bookstore-owner/
     1 <= minutes <= customers.length == grumpy.length <= 20000
     0 <= customers[i] <= 1000
     0 <= grumpy[i] <= 1
@@ -7,43 +7,14 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int satisfiedCount = 0;
-        int customerSize = customers.size();
-
-        // Start with group from index 0
-        int len = min(customerSize, minutes);
-        int unhappyCount = 0;
-        for (int i = 0; i < len; ++i) {
-            if (grumpy[i])
-                unhappyCount += customers[i];
-            else
-                satisfiedCount += customers[i];
-
+        auto satisfied = 0, m_add_satisfied = 0, add_satisfied = 0;
+        for (auto i = 0; i < cs.size(); ++i) {
+            satisfied += grumpy[i] ? 0 : cs[i];
+            add_satisfied += grumpy[i] ? cs[i] : 0;
+            if (i >= X) add_satisfied -= grumpy[i - X] ? cs[i - X] : 0;
+            m_add_satisfied = max(m_add_satisfied, add_satisfied);
         }
 
-        // Find the largest unhappy customer group
-        int highestStartIndex = 0;
-        int highestCount = unhappyCount;
-
-        for (int i = 0; i + len < customerSize; ++i) {
-            int oldVal = grumpy[i] ? customers[i] : 0;
-            int newVal = grumpy[i+len] ? customers[i+len] : 0;
-            if (!newVal)
-                satisfiedCount += customers[i+len];
-
-            unhappyCount += newVal - oldVal;
-            if (unhappyCount > highestCount) {
-                highestCount = unhappyCount;
-                highestStartIndex = i+1;
-            }
-        }
-
-        // Add the converted 'unhappy' customers
-        for (int i = 0; i < minutes; ++i) {
-            if (grumpy[i+highestStartIndex])
-                satisfiedCount += customers[i+highestStartIndex];
-        }
-
-        return satisfiedCount;
+        return satisfied + m_add_satisfied;
     }
 };
