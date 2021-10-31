409e72df65a55982f79b72dd01a22a9adaea6508
Andrew <drewkim1995@gmail.com>
2021-10-31 11:36:10 +1100

    house-robber: much better implementation

--- leetcode/198-house-robber.cpp
+++ leetcode/198-house-robber.cpp
@@ -4,26 +4,14 @@ https://leetcode.com/problems/house-robber/
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // Padded dynamic programming to prevent edge case of nums.size() == 1
-        vector<int> dp(nums.size() + 1, -1);
-        return max(houses(dp, nums, 0), houses(dp, nums, 1));
-    }
-
-    int houses(vector<int>& dp, vector<int>& nums, int startPos) {
-        int retVal = 0;
-        int subArraySize = nums.size() - startPos;
-
-        if (dp[startPos] != -1)
-            return dp[startPos];
-
-        if (subArraySize == 1)
-            retVal = nums[startPos];
-        else if(subArraySize == 2)
-            retVal = max(nums[startPos], nums[startPos+1]);
-        else if (subArraySize >= 3)
-            retVal = max(nums[startPos] + houses(dp, nums, startPos+2), houses(dp, nums, startPos+1));
-
-        dp[startPos] = retVal;
-        return retVal;
+        if (nums.size()==1) return nums[0];
+        int prevByOne=0;
+        int prevByTwo=0;
+        for (int i=0; i<nums.size(); i++) {
+            int cur=max(nums[i]+prevByTwo, prevByOne);
+            prevByTwo=prevByOne;
+            prevByOne=cur;
+        }
+        return prevByOne;
     }
 };
